e66cc5e4717084ed9ae97cdc7992d36d328929c2
BUURUUNUU <b.nunez.contato@protonmail.com>
2023-05-31 00:19:12 -0400

    update/app08

--- Projects/app08ScapeBlue/scripts/game.py
+++ Projects/app08ScapeBlue/scripts/game.py
@@ -5,12 +5,24 @@ from scripts.obj import Obj
 from scripts.scene import Scene
 from scripts.settings import *
 from scripts.text import Text
-
-class Game(Scene):
+from scripts.player import Player
+from scripts.fade import Fade
+class Game:
 
     def __init__(self):
-        super().__init__()
+       
+        self.display = pygame.display.get_surface()
+        self.all_sprites = pygame.sprite.Group()
         self.colision_sprites = pygame.sprite.Group()
+        self.active = True
+
+        self.fade = Fade(5)
+       
+       
+        
+        
+        self.player = Player([0, 0], [self.all_sprites], self.colision_sprites)
+        
         self.generate_map()
        
         
@@ -21,69 +33,17 @@ class Game(Scene):
                 y = row_index * TILE_SIZE
                 
                 if col == "x":
-                    Obj("assets/personagem/tile.png", [x, y], [self.all_sprites, self.colision_sprites])
-                elif col == "p":
-                    Player([x, y], [self.all_sprites], self.colision_sprites)
+                    Obj("assets/player/tile.png", [x, y], [self.all_sprites, self.colision_sprites])
+                
+                
+    def events(self, event):
+        pass
+                
+    def draw(self):
+        self.all_sprites.draw(self.display)
             
                 
     def update(self):
-        
-        return super().update()
+        self.all_sprites.update()
 
 
-class Player(pygame.sprite.Sprite):
-    def __init__(self,pos,  groups, colision_group):
-        super().__init__(groups)
-        
-        self.image = pygame.image.load("assets/personagem/idle.png")
-        self.rect = self.image.get_rect(topleft = pos)
-        
-        self.direction = pygame.math.Vector2()
-        self.speed = 6
-        self.gravity = 0.8
-        self.jump_force = 20
-        self.colision_group = colision_group
-        self.on_ground = False
-        
-    def input(self):
-        key = pygame.key.get_pressed()
-        
-        if key[pygame.K_a]:
-            self.direction.x = -1
-        elif key[pygame.K_d]:
-            self.direction.x = 1
-        else:
-            self.direction.x = 0
-        if key[pygame.K_SPACE] and self.on_ground:
-            self.direction.y = - self.jump_force
-            self.on_ground = False
-            
-            
-            
-    def y_colision(self):
-        for sprite in self.colision_group:
-            if sprite.rect.colliderect(self.rect):
-                if self.direction.y > 0:
-                    self.direction.y = 0
-                    self.rect.bottom = sprite.rect.top
-                    self.on_ground = True
-        
-            
-        
-    def move(self):
-        self.rect.x +=self.direction.x * self.speed
-            
-    def gravity_force(self):
-        self.direction.y += self.gravity
-        self.rect.y += self.direction.y
-        
-        
-            
-            
-    def update(self):
-        self.input()
-        self.move()
-        self.gravity_force()
-        self.y_colision()
-            
-        
